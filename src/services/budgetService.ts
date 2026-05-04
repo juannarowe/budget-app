11dd24e08c0e0ece426fc00a04503810248e7de1
juannarowe <juannarowe@gmail.com>
2026-05-04 11:29:08 +0200

    feat: add calculateTotal service function

---
+++ src/services/budgetService.ts
@@ -0,0 +1,10 @@
+import type { Service, Client, Budget } from '../types/budget.types'
+
+export function calculateTotal(services: Service[]): number {
+    return services.reduce((sum, service) => {
+        if (service.id === 'web') {
+            return sum + 500 + ((service.pages ?? 1) + (service.langs ?? 1))
+        }
+        return sum + service.price
+    }, 0)
+}
